f1ad51f3863d080d01361423ec40033eb4088bd5
Tanvir Rifath <tanvir.wazy@gmail.com>
2020-06-22 20:06:47 +0600

    Integrated bootstrap and jobs are showing up

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Blog #the class name in models.py
+
+admin.site.register(Blog)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Create A Blog model
+class Blog(models.Model):
+    title = models.CharField(max_length=250)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+#Add the blog app to the settings
+    #copy the classname from apps.py and add that app to installed apps list in main settings.py file
+#create a migration
+    #go to command prompt and type "manage.py makemigrations". This makes the new model ready for migration
+#Migrate
+    #Go to command prompt and type "manage.py migrate"
+#Add to the admin
+    #paste the same lines we have written in admin.py in jobs model to this blog model's admin.py
