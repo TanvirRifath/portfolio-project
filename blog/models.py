6102987091fba8e4d4ab566dc71d59a5447f9e2c
Tanvir Rifath <tanvir.wazy@gmail.com>
2020-07-06 23:54:36 +0600

    Portfolio Project Done with Blog App

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
+#Add the blog app to the settings
+    #copy the classname from apps.py and add that app to installed apps list in main settings.py file
+#create a migration
+    #go to command prompt and type "manage.py makemigrations". This makes the new model ready for migration
+#Migrate
+    #Go to command prompt and type "manage.py migrate"
+#Add to the admin
+    #paste the same lines we have written in admin.py in jobs model to this blog model's admin.py
+
+
 from django.db import models
 
 # Create A Blog model
@@ -7,11 +17,11 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
-#Add the blog app to the settings
-    #copy the classname from apps.py and add that app to installed apps list in main settings.py file
-#create a migration
-    #go to command prompt and type "manage.py makemigrations". This makes the new model ready for migration
-#Migrate
-    #Go to command prompt and type "manage.py migrate"
-#Add to the admin
-    #paste the same lines we have written in admin.py in jobs model to this blog model's admin.py
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%A, %b %e %Y')
